e7d823fb9d5988c9d91c3b6799db73bcc5d5cbfa
James Kidd <kklolk@gmail.com>
2017-03-10 14:06:55 +1100

    Fix non relative url's

--- gwwpchats.php
+++ gwwpchats.php
@@ -2,9 +2,9 @@
 /*
  Plugin Name: GW WPChats Integrations
  Plugin URI: 
- Description: Adds essaging buttons to profiles.
+ Description: Adds messaging buttons to profiles.
  Author: GippslandWeb
- Version: 1.0
+ Version: 1.0.2
  Author URI: https://gippslandweb.com.au
  GitHub Plugin URI: Gippsland-Web/gw-bp-wpc-integration
  */
@@ -19,14 +19,14 @@ function display_msg_button() {
 if(!is_user_logged_in())
     return;
     if(get_current_user_id() != bp_displayed_user_id())
-        echo('<div class="message-button  generic-button" id="message-button"><a href="http://localhost/wpc-messages/'.bp_core_get_username(bp_displayed_user_id()).'/">Message</a></div>');
+        echo('<div class="message-button  generic-button" id="message-button"><a href="/wpc-messages/'.bp_core_get_username(bp_displayed_user_id()).'/">Send Message</a></div>');
     else
         {
             $cnt = $this->unread_counter(get_current_user_id());
             if($cnt > 0)
-                echo('<div class="message-button  generic-button" id="message-button"><a href="http://localhost/wpc-messages/">'.$cnt.'New Message</a></div>');
+                echo('<div class="message-button  generic-button" id="message-button"><a href="/wpc-messages/">'.$cnt.' New Message</a></div>');
             else
-                    echo('<div class="message-button  generic-button" id="message-button"><a href="http://localhost/wpc-messages/">View Message</a></div>');
+                    echo('<div class="message-button  generic-button" id="message-button"><a href="/wpc-messages/">View Message</a></div>');
 
         }
     
