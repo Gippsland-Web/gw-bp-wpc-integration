0fa6d24e0de5f061ee9113ad6409340840286b5c
James Kidd <kklolk@gmail.com>
2017-03-16 17:40:04 +1100

    Styles follow button the same

--- gwwpchats.php
+++ gwwpchats.php
@@ -4,7 +4,7 @@
  Plugin URI: 
  Description: Adds messaging buttons to profiles.
  Author: GippslandWeb
- Version: 1.0.3
+ Version: 1.0.4
  Author URI: https://gippslandweb.com.au
  GitHub Plugin URI: Gippsland-Web/gw-bp-wpc-integration
  */
@@ -12,14 +12,25 @@
  class GW_WPCIntegration {
      public function __construct() {
         add_action('bp_profile_header_meta',array($this,'display_msg_button') );
+
+        add_filter('bp_follow_get_add_follow_button', array($this,'style_follow_button'),10,3);
     }
+
+// Style the Follow button in the same manner - should be moved to theme.
+function style_follow_button($button, $leader, $follower) {
+    $button["wrapper_class"] = "messagebtnholder et_pb_text et_pb_module et_pb_bg_layout_light et_pb_text_0";
+    $button['link_class'] = "messagebtn et_pb_button  et_pb_button_0 et_pb_module et_pb_bg_layout_light";
+return $button; 
+
+
+}
 
 //Display Member type in the Profile header
 function display_msg_button() {
 if(!is_user_logged_in())
     return;
     if(get_current_user_id() != bp_displayed_user_id())
-        echo('<div class="et_pb_text et_pb_module et_pb_bg_layout_light et_pb_text_0"><a class="messagebtn et_pb_button  et_pb_button_0 et_pb_module et_pb_bg_layout_light" href="/wpc-messages/'.bp_core_get_username(bp_displayed_user_id()).'/">Send Message</a></div>');
+        echo('<div class="messagebtnholder et_pb_text et_pb_module et_pb_bg_layout_light et_pb_text_0"><a class="messagebtn et_pb_button  et_pb_button_0 et_pb_module et_pb_bg_layout_light" href="/wpc-messages/'.bp_core_get_username(bp_displayed_user_id()).'/">Send Message</a></div>');
     else
         {
             $cnt = $this->unread_counter(get_current_user_id());
@@ -31,6 +42,7 @@ if(!is_user_logged_in())
         }
     
 }
+
 
 
 
